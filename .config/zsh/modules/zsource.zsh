12ab2ad27c257c45544c282844780e64d5406185
Wayne Luke <wayneluke@icloud.com>
2025-07-09 09:32:18 -0700

    Delete zsource.zsh

--- .config/zsh/modules/zsource.zsh
+++
@@ -1,9 +0,0 @@
-function zsource() {
-  local file=$1
-  local zwc="${file}.zwc"
-
-  if [[ -f "$file" && (! -f "$zwc" || "$file" -nt "$zwc") ]]; then
-    zcompile "$file"
-  fi
-  source "$file"
-}
